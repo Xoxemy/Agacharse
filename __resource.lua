7111b4d6f59bf593547a0f6628002293401e2083
Xoxemy <76178594+Xoxemy@users.noreply.github.com>
2023-05-17 17:48:12 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+--##############################
+--#                            #
+--# CONFIG. BY -AGUS. AP LEAKS #
+--#                            #
+--##############################
+--# Agacharse
+--# Dudas al privado: -Agus#8423
+--# Mi servidor de Discord -> https://discord.gg/JXCdVPcDG4
+
+-- Set the resource manifest 
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+-- Add a client script 
+client_script 'client.lua'
